9d101bdeb02d29231804c9d1b508b010931970cb
JinmingZhuang <m13688033757@163.com>
2022-11-12 17:27:20 -0800

    Update int16 single kernel

--- src_gen/Kernel_Gen/gen_gracpp.sh
+++ src_gen/Kernel_Gen/gen_gracpp.sh
@@ -13,11 +13,11 @@ else
     exit
 fi
 
-if [ -f ./${dir_name}/aie/aie_graph.cpp ]
+if [ -f ./${dir_name}/aie/mm_top.cpp ]
 then
     echo ""
     echo "******************************************"
-    echo "File ./${dir_name}/aie/aie_graph.cpp exists." 
+    echo "File ./${dir_name}/aie/mm_top.cpp exists." 
     echo "******************************************"
     echo ""
     exit;
@@ -64,7 +64,7 @@ then
 fi
 
 mkdir -p ${dir_name}/aie
-if [ ${data_type} == "int32" ] && [ ${kernel_type} == 0 ]
+if [ ${kernel_type} == 0 ]
 then
 echo \
 "#include "'"aie_graph.h"'"
@@ -91,100 +91,9 @@ int main(int argc, char** argv) {
     return 0;
 }
 #endif
-">> ./${dir_name}/aie/aie_graph.cpp;
+">> ./${dir_name}/aie/mm_top.cpp;
 
-elif [ ${data_type} == "int32" ] && [ ${kernel_type} == 1 ]
-then
-echo \
-"#include "'"aie_graph.h"'"
-using namespace adf;
-
-PLIO* in0 = new PLIO("'"DataIn0"'", adf::plio_32_bits, \"../${din_name}input0.txt\",1000);
-PLIO* in1 = new PLIO("'"DataIn1"'", adf::plio_32_bits, \"../${din_name}input1.txt\",1000);
-PLIO* in2 = new PLIO("'"DataIn2"'", adf::plio_32_bits, \"../${din_name}input0.txt\",1000);
-PLIO* in3 = new PLIO("'"DataIn3"'", adf::plio_32_bits, \"../${din_name}input1.txt\",1000);
-PLIO* out = new PLIO("'"DataOut"'", adf::plio_32_bits, "'"data/output.txt"'",1000);
-
-simulation::platform<4, 1> platform(in0, in1, in2, in3, out);
-
-simpleGraph addergraph;
-
-connect<> net0(platform.src[0], addergraph.in0);
-connect<> net1(platform.src[1], addergraph.in1);
-connect<> net2(platform.src[2], addergraph.in2);
-connect<> net3(platform.src[3], addergraph.in3);
-
-connect<> net4(addergraph.out, platform.sink[0]);
-
-#ifdef __AIESIM__
-int main(int argc, char** argv) {
-    addergraph.init();
-    addergraph.run(1);
-    addergraph.end();
-    return 0;
-}
-#endif
-">> ./${dir_name}/aie/aie_graph.cpp;
-
-elif [ ${data_type} == "fp32" ] && [ ${kernel_type} == 0 ]
-then
-echo \
-"#include \"aie_graph.h\"
-using namespace adf;
-
-PLIO* in0 = new PLIO(\"DataIn0\", adf::plio_32_bits, \"../${din_name}input0.txt\",1000);
-PLIO* in1 = new PLIO(\"DataIn1\", adf::plio_32_bits, \"../${din_name}input1.txt\",1000);
-PLIO* out = new PLIO(\"DataOut\", adf::plio_32_bits, \"data/output.txt\",1000);
-
-simulation::platform<2, 1> platform(in0, in1, out);
-
-simpleGraph addergraph;
-
-connect<> net0(platform.src[0], addergraph.in0);
-connect<> net1(platform.src[1], addergraph.in1);
-
-connect<> net2(addergraph.out, platform.sink[0]);
-
-#ifdef __AIESIM__
-int main(int argc, char** argv) {
-    addergraph.init();
-    addergraph.run(1);
-    addergraph.end();
-    return 0;
-}
-#endif
-">> ./${dir_name}/aie/aie_graph.cpp;
-
-elif [ ${data_type} == "fp32" ] && [ ${kernel_type} == 0 ]
-then
-echo \
-"#include \"aie_graph.h\"
-using namespace adf;
-
-PLIO* in0 = new PLIO(\"DataIn0\", adf::plio_32_bits, \"../${din_name}input0.txt\",1000);
-PLIO* in1 = new PLIO(\"DataIn1\", adf::plio_32_bits, \"../${din_name}input1.txt\",1000);
-PLIO* out = new PLIO(\"DataOut\", adf::plio_32_bits, \"data/output.txt\",1000);
-
-simulation::platform<2, 1> platform(in0, in1, out);
-
-simpleGraph addergraph;
-
-connect<> net0(platform.src[0], addergraph.in0);
-connect<> net1(platform.src[1], addergraph.in1);
-
-connect<> net2(addergraph.out, platform.sink[0]);
-
-#ifdef __AIESIM__
-int main(int argc, char** argv) {
-    addergraph.init();
-    addergraph.run(1);
-    addergraph.end();
-    return 0;
-}
-#endif
-">> ./${dir_name}/aie/aie_graph.cpp;
-
-elif [ ${data_type} == "fp32" ] && [ ${kernel_type} == 1 ]
+elif [ ${kernel_type} == 1 ]
 then
 echo \
 "#include "'"aie_graph.h"'"
@@ -215,6 +124,5 @@ int main(int argc, char** argv) {
     return 0;
 }
 #endif
-">> ./${dir_name}/aie/aie_graph.cpp;
-
+">> ./${dir_name}/aie/mm_top.cpp;
 fi
--- src_gen/Kernel_Gen/gen_grah.sh
+++ src_gen/Kernel_Gen/gen_grah.sh
@@ -76,10 +76,13 @@ then
 elif [ ${data_type} == "int16" ]
 then
 	BPE=2;
+elif [ ${data_type} == "int8" ]
+then
+	BPE=1;
 fi
 
 mkdir -p ${dir_name}/aie
-if [ ${data_type} == "int32" ] && [ ${kernel_type} == 0 ]
+if [ ${kernel_type} == 0 ]
 then
 echo \
 "#ifndef __GRAPH_H__
@@ -114,119 +117,7 @@ class simpleGraph : public graph {
 #endif
 ">> ./${dir_name}/aie/aie_graph.h;
 
-elif [ ${data_type} == "int32" ] && [ ${kernel_type} == 1 ]
-then
-echo \
-"#ifndef __GRAPH_H__
-#define __GRAPH_H__
-
-#include <adf.h>
-#include "'"para.h"'"
-using namespace adf;
-
-class simpleGraph : public graph {
-   private:
-    kernel mm;
-    kernel mm1;
-
-   public:
-    port<input> in0, in1, in2, in3;
-    port<output> out;
-
-    simpleGraph() {
-        mm = kernel::create(mm_kernel0);
-        mm1 = kernel::create(mm_kernel1);
-
-        connect<window<h1*w1*${BPE}>>(in0, mm.in[0]);
-        connect<window<w1*w2*${BPE}>>(in1, mm.in[1]);
-        connect<window<h1*w1*${BPE}>>(in2, mm1.in[0]);
-        connect<window<w1*w2*${BPE}>>(in3, mm1.in[1]);
-        connect<window<h1*w2*${BPE}>>(mm.out[0], mm1.in[2]);
-        connect<window<h1*w2*${BPE}>>(mm1.out[0], out);
-
-
-
-        source(mm) = "'"mm_kernel0.cc"'";
-        source(mm1) = "'"mm_kernel1.cc"'";
-        runtime<ratio>(mm) = 1;
-        runtime<ratio>(mm1) = 1;
-    };
-};
-
-#endif
-">> ./${dir_name}/aie/aie_graph.h;
-
-elif [ ${data_type} == "fp32" ] && [ ${kernel_type} == 0 ]
-then
-echo \
-"#ifndef __GRAPH_H__
-#define __GRAPH_H__
-
-#include <adf.h>
-#include \"para.h\"
-using namespace adf;
-
-class simpleGraph : public graph {
-   private:
-    kernel mm;
-
-   public:
-    port<input> in0, in1;
-    port<output> out;
-
-    simpleGraph() {
-        mm = kernel::create(mm_kernel0);
-
-        connect<window<h1*w1*${BPE}>>(in0, mm.in[0]);
-        connect<window<w1*w2*${BPE}>>(in1, mm.in[1]);
-        connect<window<h1*w2*${BPE}>>(mm.out[0], out);
-
-
-        source(mm) = \"mm_kernel0.cc\";
-
-        runtime<ratio>(mm) = 1;
-    };
-};
-
-#endif
-">> ./${dir_name}/aie/aie_graph.h;
-
-elif [ ${data_type} == "fp32" ] && [ ${kernel_type} == 0 ]
-then
-echo \
-"#ifndef __GRAPH_H__
-#define __GRAPH_H__
-
-#include <adf.h>
-#include \"para.h\"
-using namespace adf;
-
-class simpleGraph : public graph {
-   private:
-    kernel mm;
-
-   public:
-    port<input> in0, in1;
-    port<output> out;
-
-    simpleGraph() {
-        mm = kernel::create(mm_kernel0);
-
-        connect<window<h1*w1*${BPE}>>(in0, mm.in[0]);
-        connect<window<w1*w2*${BPE}>>(in1, mm.in[1]);
-        connect<window<h1*w2*${BPE}>>(mm.out[0], out);
-
-
-        source(mm) = \"mm_kernel0.cc\";
-
-        runtime<ratio>(mm) = 1;
-    };
-};
-
-#endif
-">> ./${dir_name}/aie/aie_graph.h;
-
-elif [ ${data_type} == "fp32" ] && [ ${kernel_type} == 1 ]
+elif [ ${kernel_type} == 1 ]
 then
 echo \
 "#ifndef __GRAPH_H__
@@ -267,5 +158,4 @@ class simpleGraph : public graph {
 
 #endif
 ">> ./${dir_name}/aie/aie_graph.h;
-
 fi
